b0efc6e0005444b98078b08d5d265c8a53bf5448
gh-andre <gh-andre@users.noreply.github.com>
2022-11-23 15:45:09 -0500

    Reworked CMake files to use configuration options
    * Added `fmtlog` headers to the list of installed files.
    * Set up CMake install variables to point to GNU installation
      directories, which detects `lib` vs. `lib64`, etc.
    * Added CMake options to build tests conditionally.
    * Debug `fmtlog` libraries reference debug `fmt` libraries and
      expect to find debug functions, such as `assert_fail`. Release
      libraries are built with `NDEBUG` and don't include references
      to such functions.
    * Library names have changed to use CMake's built-in functionality
      to generate debug library suffixes and other similar constructs.
    * Added `fmt` headers to the list of install files when `fmt` is
      bundled with `fmtlog`.
    * Added an error if a shared library is attempted to be built
      on Windows because the source doesn't export symbols.
    * Updated build.sh to build a release library with tests.

--- build.sh
+++ build.sh
@@ -1,7 +1,14 @@
+#!/bin/bash
+
 BUILD_DIR=${BUILD_DIR:-.build}
 
+# Git submodule fmt must be initialized (see CMakeLists.txt for details)
 mkdir -p "$BUILD_DIR" \
   && cd "$BUILD_DIR" \
-  && cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..\
+  && cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+       -DCMAKE_BUILD_TYPE=Release \
+       -DFMTLOG_BUILD_TEST=ON \
+       -DFMTLOG_BUNDLED_FMT=ON \
+       ..\
   && cmake --build . -j \
   && cmake --install . --prefix .
---
+++ get-fmt.bat
@@ -0,0 +1,25 @@
+@echo off
+
+setlocal
+
+set FMT_VER=9.1.0
+set BLD_DIR=%~1
+
+if "%BLD_DIR%" == "" set BLD_DIR=build
+
+curl --location --output fmt-%FMT_VER%.tar.gz https://github.com/fmtlib/fmt/archive/refs/tags/%FMT_VER%.tar.gz
+                                            
+tar -xzf fmt-%FMT_VER%.tar.gz
+
+del /Q fmt-%FMT_VER%.tar.gz
+
+cmake -S fmt-%FMT_VER% -B %BLD_DIR%/fmt-build -DFMT_DOC=OFF -DFMT_TEST=OFF
+
+cmake --build %BLD_DIR%/fmt-build --config Debug
+cmake --build %BLD_DIR%/fmt-build --config Release
+
+cmake --install %BLD_DIR%/fmt-build --config Debug --prefix %BLD_DIR%/fmt
+cmake --install %BLD_DIR%/fmt-build --config Release --prefix %BLD_DIR%/fmt
+
+rmdir /S /Q fmt-%FMT_VER%
+rmdir /S /Q %BLD_DIR%\fmt-build
